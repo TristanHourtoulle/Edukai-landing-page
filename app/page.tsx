9f691a839d3ffe29f6a5812dc26a3118c34ac82f
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-09 16:53:37 +0800

    feat: add First version of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,62 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="outfit-regular w-full h-full flex flex-col gap-6 items-center justify-center pb-[3%]">
+      <div className="flex flex-col gap-3 items-center justify-center text-center outfit-regular">
+        {/* <Badge className="flex items-center justify-center gap-4 outfit-regular text-xs text-primary-500 px-[3%] py-[0.5%] rounded-full border border-primary-500 bg-primary-500 bg-opacity-25 hover:bg-primary-500 hover:bg-opacity-25">
+          <Image src={"/icons/stars.svg"} width={15} height={15} alt="Stars" />
+          Dès Juillet 2025
+        </Badge> */}
+        <h1 className="outfit-semibold text-7xl text-white w-full">
+          L'IA au service de vos révisions
+        </h1>
+        <p className="text-white text-opacity-75 text-lg text-center outfit-light w-full px-[10%]">
+          Envoyez vos documents, notre outil les transforme en fiches et quiz
+          sur-mesure.<br></br>
+          <span className="text-primary-200">
+            Révisez efficacement, sans effort.
+          </span>
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="flex items-center justify-center gap-5 w-full outfit-regular mt-2">
+        <Button className="text-white py-3 px-8 rounded-full shadow-xl transition-all transform hover:scale-105 relative">
+          Rester informé
+        </Button>
+
+        <Button className="text-blue-500 py-3 px-8 rounded-full shadow-md transition-all bg-primary-500 bg-opacity-10 border-2 border-primary-500 hover:bg-primary-300 hover:bg-opacity-10">
+          Nous contacter
+        </Button>
+      </div>
+
+      <div className="flex items-center justify-center gap-5 mt-[2%]">
+        <Image
+          src={"/screens/LP - 2.svg"}
+          width={200}
+          height={200}
+          alt="Illustration"
+          className="shadow-lg transition-all transform hover:scale-105"
+        />
+        <Image
+          src={"/screens/LP - 4.svg"}
+          width={400}
+          height={400}
+          alt="Illustration"
+          className="shadow-lg transition-all transform hover:scale-105 rounded-lg"
+          style={{
+            boxShadow: "0 4px 20px rgba(54, 120, 255, 0.5)", // Ombre colorée
+          }}
+        />
+        <Image
+          src={"/screens/LP - 3.svg"}
+          width={200}
+          height={200}
+          alt="Illustration"
+          className="shadow-lg transition-all transform hover:scale-105"
+        />
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,62 +1,101 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#3678FF",
+          50: "#F0F5FF",
+          100: "#D9E4FF",
+          200: "#AABFFF",
+          300: "#7B99FF",
+          400: "#4C73FF",
+          500: "#3678FF",
+          600: "#1E4CFF",
+          700: "#163A99",
+          800: "#0F294D",
+          900: "#081A33",
+        },
+        secondary: {
+          DEFAULT: "#0A162F",
+          50: "#F0F2F7",
+          100: "#D9DCED",
+          200: "#ABB0D8",
+          300: "#7C85C3",
+          400: "#4E59AE",
+          500: "#0A162F",
+          600: "#1E4CFF",
+          700: "#163A99",
+          800: "#0F294D",
+          900: "#081A33",
+        },
+        accent: {
+          DEFAULT: "#FF9636",
+          50: "#FFF8F0",
+          100: "#FFE9D9",
+          200: "#FFCBA9",
+          300: "#FFAD79",
+          400: "#FF8F49",
+          500: "#FF9636",
+          600: "#FF6A0E",
+          700: "#994F0B",
+          800: "#4D2F07",
+          900: "#2E1F05",
+        },
+        // card: {
+        // 	DEFAULT: 'hsl(var(--card))',
+        // 	foreground: 'hsl(var(--card-foreground))'
+        // },
+        // popover: {
+        // 	DEFAULT: 'hsl(var(--popover))',
+        // 	foreground: 'hsl(var(--popover-foreground))'
+        // },
+        // primary: {
+        // 	DEFAULT: 'hsl(var(--primary))',
+        // 	foreground: 'hsl(var(--primary-foreground))'
+        // },
+        // secondary: {
+        // 	DEFAULT: 'hsl(var(--secondary))',
+        // 	foreground: 'hsl(var(--secondary-foreground))'
+        // },
+        // muted: {
+        // 	DEFAULT: 'hsl(var(--muted))',
+        // 	foreground: 'hsl(var(--muted-foreground))'
+        // },
+        // accent: {
+        // 	DEFAULT: 'hsl(var(--accent))',
+        // 	foreground: 'hsl(var(--accent-foreground))'
+        // },
+        // destructive: {
+        // 	DEFAULT: 'hsl(var(--destructive))',
+        // 	foreground: 'hsl(var(--destructive-foreground))'
+        // },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };

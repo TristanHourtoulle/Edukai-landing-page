0e28095e9386046a3d9255202444800ba8b92758
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-10 13:07:02 +0800

    refactor: put 'tu' instead of 'vous'

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,20 @@ export default function Home() {
           Dès Juillet 2025
         </Badge> */}
         <h1 className="outfit-semibold text-7xl text-white w-full">
-          L'IA au service de vos révisions
+          L'IA au service de tes révisions
         </h1>
         <p className="text-white text-opacity-75 text-lg text-center outfit-light w-full px-[10%]">
-          Envoyez vos documents, notre outil les transforme en fiches et quiz
+          Envoye tes documents, notre outil les transforme en fiches et quiz
           sur-mesure.<br></br>
           <span className="text-primary-200">
-            Révisez efficacement, sans effort.
+            Révise efficacement, sans effort.
           </span>
         </p>
       </div>
 
       <div className="flex items-center justify-center gap-5 w-full outfit-regular mt-2">
         <Button className="text-white py-3 px-8 rounded-full shadow-xl transition-all transform hover:scale-105 relative">
-          Rester informé
+          Reste informé
         </Button>
 
         <Button className="text-blue-500 py-3 px-8 rounded-full shadow-md transition-all bg-primary-500 bg-opacity-10 border-2 border-primary-500 hover:bg-primary-300 hover:bg-opacity-10">
